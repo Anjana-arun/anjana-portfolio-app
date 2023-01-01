5c85d20da552afd1b686642b306c269d170d7b3f
Anjana Arun <anjoo.pilai@gmail.com>
2023-01-01 22:15:11 +0100

    Add hosting and HTTPS configuration

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Under Construction
+          Hosted in netlify
         </p>        
       </header>
     </div>
